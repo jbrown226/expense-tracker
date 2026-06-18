81ad3ceb0ee75f5aa37546a2b32a89b6b06cc6cb
sixplanet312 <vulturej372@gmail.com>
2026-06-17 21:02:34 -0700

    removed jacoco and junit

--- src/main/java/com/expensetracker/Main.java
+++ src/main/java/com/expensetracker/Main.java
@@ -3,7 +3,7 @@ package com.expensetracker;
 public class Main {  
     public static void main(String[] args) {
                 
-        System.out.println("Expense tracker here");
+        System.out.println("Expense tracker is running...");
         App.main(args);
 
     }
